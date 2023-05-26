00a71936e4dac9d8cc225e0a5af3a19291ecd989
yoodab <yoondab2166@gmail.com>
2023-05-26 12:41:08 +0900

    35. OAuth2 페이스북 로그인(긍록, 설정, 정보받기)

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -7,10 +7,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @EnableWebSecurity // 해당파일로 시큐리티 활성화
 @Configuration // IoC등록
 public class SecurityConfig {
 
+	private final OAuth2DetailsService oAuth2DetailsService;
+	
 	@Bean
 	BCryptPasswordEncoder encode() {
 		return new BCryptPasswordEncoder();
@@ -29,7 +36,11 @@ public class SecurityConfig {
 			.formLogin()
 			.loginPage("/auth/signin") // GET요청시
 			.loginProcessingUrl("/auth/signin") // POST -> 스프링 시큐리티가 로그인 프로세스 진행 -> UserDetailsService
-			.defaultSuccessUrl("/");
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login() // form로그인도 하는데, oauth2 로그인도 사용함
+			.userInfoEndpoint() // oauth2 로그인을 하면 최종 응답을 회원 정보로 받을 수 있다.
+			.userService(oAuth2DetailsService);
 		return http.build();
 	}
 	
